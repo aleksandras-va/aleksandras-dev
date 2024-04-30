cbc26f7c56d4f0f120596a549cc0ccda9ae2111f
Aleksandras Vasinas <aleksandrasvasinas@gmail.com>
2024-04-30 22:57:06 +0300

    feat: add google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aleksandras.dev',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
