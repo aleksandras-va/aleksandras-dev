ad5903b32c3badfb648d422202f8d790351da689
Aleksandras Vasinas <aleksandrasvasinas@gmail.com>
2024-04-30 22:41:12 +0300

    fix: og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon()]
+  site: 'https://aleksandras.dev',
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
